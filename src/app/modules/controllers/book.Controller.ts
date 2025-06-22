cd72c48cbdac9bf5c9a80226d7e0aa22ff5776fa
Rabiul Islam <rabiul.robi2023@gmail.com>
2025-06-22 20:32:58 +0600

    All operation on books completed

--- src/app/modules/controllers/book.Controller.ts
+++ src/app/modules/controllers/book.Controller.ts
@@ -3,6 +3,7 @@ import { Books } from "../model/book.model";
 
 export const booksRouter = Router();
 
+//===========================Create Book=============================
 booksRouter.post("/", async (req: Request, res: Response) => {
   try {
     const bookData = req.body;
@@ -22,25 +23,94 @@ booksRouter.post("/", async (req: Request, res: Response) => {
   }
 });
 
+//===========================Get All Book=============================
 booksRouter.get("/", async (req: Request, res: Response) => {
   try {
     const filter = req.query?.filter;
     const sortBy = req.query?.sortBy;
     const sort = req.query?.sort;
-    const limit:number = Number(req.query.limit)||10
+    const limit: number = Number(req.query.limit) || 10;
 
     const sortOption: any = sortBy
       ? { [sortBy as string]: sort === "desc" ? -1 : 1 }
       : { title: 1 };
 
-    const result = await Books.find(filter ? { genre: filter } : {}).sort(
-      sortOption
-    ).limit(limit);
+    const result = await Books.find(filter ? { genre: filter } : {})
+      .sort(sortOption)
+      .limit(limit);
 
     res.status(200).json({
       success: true,
       message: "Books retrieved successfully",
       data: result,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      message: error?.name,
+      success: false,
+      error: error.errors,
+    });
+  }
+});
+
+//===========================Get Single Book=============================
+booksRouter.get("/:bookId", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.bookId;
+    const result = await Books.findById(id);
+    if (result) {
+      res.status(200).json({
+        success: true,
+        message: "Book retrieved successfully",
+        data: result,
+      });
+    }
+    else{
+         res.status(404).json({
+        success: false,
+        message: "There is no any books",
+        data: null,
+      });
+    }
+  } catch (error: any) {
+    res.status(400).json({
+      message: error?.name,
+      success: false,
+      error: error.errors,
+    });
+  }
+});
+
+//===========================Update A Book=============================
+booksRouter.put("/:bookId", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.bookId;
+    const updateData = req.body;
+    const result = await Books.findByIdAndUpdate(id, updateData, { new: true });
+    res.status(200).json({
+      success: true,
+      message: "Book updated successfully",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      message: error?.name,
+      success: false,
+      error: error.errors,
+    });
+  }
+});
+
+//===========================Delete A Book=============================
+booksRouter.delete("/:bookId", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.bookId;
+    const updateData = req.body;
+    const result = await Books.findByIdAndDelete(id, updateData);
+    res.status(200).json({
+      success: true,
+      message: "Book deleted successfully",
+      data: null,
     });
   } catch (error: any) {
     res.status(400).json({
